5044e9533f22c26594cf5c20f0bf2cb2672718ea
Domenico Giambra <sviluppo@editt.it>
2021-01-25 11:38:32 +0100

    Popolo le nuove tabelle

--- Migrations/Migration_20210125105700_AllTablesCustom.php
+++ Migrations/Migration_20210125105700_AllTablesCustom.php
@@ -18,5 +18,13 @@ class Migration_20210125105700_AllTablesCustom extends Migration
 		$this->addColumn('travio_stations_custom_texts', 'lang', ['type' => 'char(2)', 'null' => false]);
 		$this->addIndex('travio_stations_custom_texts', 'travio_stations_custom_texts_idx', ['parent']);
 		$this->addForeignKey('travio_stations_custom_texts', 'travio_stations_custom_texts', 'parent', 'travio_stations_custom', 'id', ['on-delete' => 'CASCADE']);
+
+		$this->query('INSERT INTO `travio_tags_custom`(`id`) SELECT `id` FROM `travio_tags`');
+		$this->query('INSERT INTO `travio_tags_types_custom`(`id`) SELECT `id` FROM `travio_tags_types`');
+		$this->query('INSERT INTO `travio_orders_custom`(`id`) SELECT `id` FROM `travio_orders`');
+		$this->query('INSERT INTO `travio_airports_custom`(`id`) SELECT `id` FROM `travio_airports`');
+		$this->query('INSERT INTO `travio_ports_custom`(`id`) SELECT `id` FROM `travio_ports`');
+		$this->query('INSERT INTO `travio_stations_custom`(`id`) SELECT `id` FROM `travio_stations`');
+		$this->query('INSERT INTO `travio_stations_custom_texts`(`id`,`parent`,`lang`) SELECT `id`,`parent`,`lang` FROM `travio_stations_texts`');
 	}
 }
