63b7cc1b0880e38f2c0dc810f20663e8501e670b
Domenico Giambra <zorinik@gmail.com>
2021-07-28 09:47:11 +0200

    Aggiungo le righe preesistenti

--- Migrations/Migration_20210728093900_ClassificazioniCustom.php
+++ Migrations/Migration_20210728093900_ClassificazioniCustom.php
@@ -7,5 +7,7 @@ class Migration_20210728093900_ClassificazioniCustom extends Migration
 	public function exec()
 	{
 		$this->createTable('travio_classifications_custom');
+
+		$this->query('INSERT INTO `travio_classifications_custom`(`id`) SELECT `id` FROM `travio_classifications`');
 	}
 }
