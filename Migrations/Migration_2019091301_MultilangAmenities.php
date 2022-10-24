6475a4563af90e1e6c5f5d413b97778118aa7c4a
Domenico Giambra <zorinik@gmail.com>
2022-10-24 22:51:10 +0200

    Passo a nuova libreria per la proprietà langs

--- Migrations/Migration_2019091301_MultilangAmenities.php
+++ Migrations/Migration_2019091301_MultilangAmenities.php
@@ -1,6 +1,7 @@
 <?php namespace Model\Travio\Migrations;
 
 use Model\Db\Migration;
+use Model\Multilang\Ml;
 
 class Migration_2019091301_MultilangAmenities extends Migration
 {
@@ -13,9 +14,8 @@ class Migration_2019091301_MultilangAmenities extends Migration
 		$this->addIndex('travio_amenities_texts', 'travio_amenities_texts_idx', ['parent']);
 		$this->addForeignKey('travio_amenities_texts', 'travio_amenities_texts', 'parent', 'travio_amenities', 'id', ['on-delete' => 'CASCADE']);
 
-		foreach ($this->model->_Multilang->langs as $lang) {
+		foreach (Ml::getLangs() as $lang)
 			$this->query('INSERT INTO travio_amenities_texts(`parent`,`lang`,`name`) SELECT id,\'' . $lang . '\',`name` FROM travio_amenities');
-		}
 
 		$this->dropColumn('travio_amenities', 'name');
 	}
