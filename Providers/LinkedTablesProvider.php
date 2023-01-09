5134d8dbb0dd6ba84291400412bf7008db18d4a8
Domenico Giambra <zorinik@gmail.com>
2023-01-09 19:50:11 +0100

    Adattamento a nuova versione libreria

--- Providers/LinkedTablesProvider.php
+++ Providers/LinkedTablesProvider.php
@@ -4,24 +4,22 @@ use Model\LinkedTables\AbstractLinkedTablesProvider;
 
 class LinkedTablesProvider extends AbstractLinkedTablesProvider
 {
-	public static function tables(): array
+	public static function tables(\Model\Db\DbConnection $db): array
 	{
 		return [
-			'primary' => [
-				'travio_geo',
-				'travio_services',
-				'travio_packages',
-				'travio_tags',
-				'travio_orders',
-				'travio_airports',
-				'travio_ports',
-				'travio_stations',
-				'travio_master_data',
-				'travio_payment_methods',
-				'travio_payment_conditions',
-				'travio_classifications',
-				'travio_packages_departures',
-			],
+			'travio_geo',
+			'travio_services',
+			'travio_packages',
+			'travio_tags',
+			'travio_orders',
+			'travio_airports',
+			'travio_ports',
+			'travio_stations',
+			'travio_master_data',
+			'travio_payment_methods',
+			'travio_payment_conditions',
+			'travio_classifications',
+			'travio_packages_departures',
 		];
 	}
 }
